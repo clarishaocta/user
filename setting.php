4bf6830241a6963e33abeabcde10a94ac6bffc88
clarisha octa viany phiranti <57120890+clarishaocta@users.noreply.github.com>
2019-12-02 15:49:44 +0700

    clarishaocta.github.io

---
+++ setting.php
@@ -0,0 +1,227 @@
+<?php
+/* setting bot */
+$ads=$ijo."    [ official facebook clarisha octa ]".$t.$red."\t >> follow facebook << ".$t.$pth."[ ".$ijo."AW 2000".$pth." ]=[".$kn." Clarisha Octa ".$pth."]=[ ".$tr."HĂN C_GAMER ]".$t;
+
+$donasi=false;
+
+$warn=$red."[!] warning !!!
+".$kn."[-] ini adalah program ilegal
+".$kn."[-] resiko sepenuhnya di tanggung pengguna".$t;
+
+$key="clarisha";
+$link_yogo="https://youtu.be/hLkAqZM3QHc";
+
+$baca_plus=$red."
+──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$kuning █▀▄ █── █─█ ▄▀▀────$red
+──── █▀█ █▀█ █─ █▀█────$kuning █─█ █─▄ █─█ ─▀▄────$red
+──── ▀▀─ ▀─▀ ─▀ ▀─▀────$kuning █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
+author:$ijo clarisha$putih || code invite:$ijo F249nlMzGk$putih
+facebook :$kuning medusa$putih || code invite:$ijo F249nlMzGk".$t;
+$msg_baca= "[•] official facebook clarisha octa".$t."[•] update skrip fix error 1/12/2019";
+$stat_baca=true;
+ 
+$ccu= $red."
+──── ▄▀ ▄▀▄ ▀ █▄─█──── ▄▀ █── █─█ █▀▄────".$pth."
+──── █─ █─█ █ █─▀█──── █─ █─▄ █─█ █▀█────
+──── ─▀ ─▀─ ▀ ▀──▀──── ─▀ ▀▀▀ ─▀─ ▀▀─────
+author: ".$ijo."clarisha ".$pth."|| facebook: ".$ijo."clarisha octa".$t;
+$msg_ccu=$ijo."[•] official facebook clarisha octa dan medusa";
+$stat_ccu=true;
+
+$mc=$br."
+──── █▄─▄█ ▄▀▄ █▄─█ █▀▀ ▀▄─▄▀──── ▄▀ █─█ █▀▄ █▀▀────
+──── █─█─█ █─█ █─▀█ █▀▀ ──█────── █─ █─█ █▀█ █▀▀────
+──── ▀───▀ ─▀─ ▀──▀ ▀▀▀ ──▀────── ─▀ ─▀─ ▀▀─ ▀▀▀────
+".$pth."author: ".$ijo."clarisha ".$pth."|| ".$ijo."follow facebook clarisha octa".$t.$pth."invite : ".$ijo."YCUSP ".$pth."|| ".$ijo."W9HVQ
+".$pth."support: ".$ijo."follow facebook medusa".$t;
+$msg_mc=$ijo."[•] This script doesn't work".$t."[•] skrip coid";
+$stat_mc=true;
+
+$flash="$red
+ █▀──── █────── ▄▀▄──── ▄▀▀──── █────── ▄▀▀───── ▄▀▄
+ █▀──── █─▄──── █▀█──── ─▀▄──── █▀▄──── █─▀▌──── █─█
+ ▀───── ▀▀▀──── ▀─▀──── ▀▀───── ▀─▀──── ▀▀▀───── ─▀─
+$putih  author :$ijo clarisha$putih ||||| facebook :$turkis clarisha octa".$t."$putih  support :$ijo medusa$putih ||||| facebook :$turkis medusa";
+$msg_flash=$turkis."[•] flash go";
+$stat_flash=true;
+
+$yogov= $kuning."
+ ▀▄─▄▀ ▄▀▄ ▄▀▀─ ▄▀▄──── ▐▌─▐▌ ▀ █▀▄ █▀▀ ▄▀▄
+ ──█── █─█ █─▀▌ █─█──── ─▀▄▀─ █ █─█ █▀▀ █─█
+ ──▀── ─▀─ ▀▀▀─ ─▀───── ──▀── ▀ ▀▀─ ▀▀▀ ─▀─".$t.
+$ijo."[ v 1.0 ] ".$putih."author: ".$ijo."clarisha".$t.">> follow facebook clarisha octa".$t.">> follow facebook medusa".$t;
+$msg_yogo=$ijo."[•] enjoy your life";
+$stat_yogo=true;
+$yogo=["tes"];
+
+$wot=$pth."
+    ___ Wheel __       __O_f__        _Trust__".$red."
+    __ |     / / ".$kn."      __  __ \ ".$ijo."      ___  __/".$red."
+    __ | /| / /  ".$kn."      _  / / / ".$ijo."      __  /   ".$red."
+    __ |/ |/ /  ".$kn."       / /_/ /  ".$ijo."      _  /    ".$red."
+    ____/|__/   ".$kn."       \____/   ".$ijo."      /_/     ".$pth."
+========[ v 1.0 ][author: clarisha]========".$t;
+$stat_wot=false;
+$msg_wot=$ijo."[•] skrip dalam perbaikan";
+
+
+$emon=$ijo."
+──── █▀▀──── █▄─▄█──── ▄▀▄──── █▄─█────
+──── █▀▀──── █─█─█──── █─█──── █─▀█────
+──── ▀▀▀──── ▀───▀──── ─▀───── ▀──▀────".$pth."
+==[author:".$ijo." clarisha".$pth."]=|=".$tr."[ v : 1 . 0 ]".$pth."==".$t;
+$stat_emon=true;
+$auto_ref=true;
+$msg_emon= $ijo."[•] enjoy your life";
+
+$you_me=$red."
+──── ▀▄─▄▀ ▄▀▄ █─█──── █▄─▄█ █▀▀────
+──── ──█── █─█ █─█──── █─█─█ █▀▀────
+──── ──▀── ─▀─ ─▀───── ▀───▀ ▀▀▀────".$t.$pth."========".$ijo."[author : clarisha]".$pth."========".$t;
+$youme=true;
+$msg_you=$ijo."[•] enjoy your life";
+
+$ltc=$red."
+_______________________________________
+    _| [".$ijo."ROOT".$red."]  _|  ".$ijo." |||||||||||||||||||".$red."                   
+    _|       _|_|_|_|       _|_|_|      
+    _|         _|         _|            
+    _|  __     _|         _|         
+_____|____|__".$ijo."I".$red."__|_|___".$ijo."E".$red."____|_|_|_|_".$ijo."OIN_".$pth."
+[ v.1.0 ]========[ author : clarisha ] ".$t;
+$msg_ltc="[•] enjoy your life";
+$do_ltc=false;
+$stat_ltc=true;
+
+$eth_far=$tr."
+──── █▀▀ ▀█▀ █────── █▀ ▄▀▄ █▀▀▄ █▄─▄█ █▀▀ █▀▀▄────
+──── █▀▀ ─█─ █▀▄──── █▀ █▀█ █▐█▀ █─█─█ █▀▀ █▐█▀────
+──── ▀▀▀ ─▀─ ▀─▀──── ▀─ ▀─▀ ▀─▀▀ ▀───▀ ▀▀▀ ▀─▀▀────
+======[".$ijo." version 1.0".$tr." ]===[ author:".$ijo." clarisha".$tr." ]======".$t;
+$msg_eth_far=$ijo."[•] enjoy your life";
+$stat_eth_far=true;
+
+$ice=$br."
+──── ▀ ▄▀ █▀▀──── █▀▄ █▀▀▄ █▀▀ ▄▀▄ █─▄▀ █▀▀ █▀▀▄────
+──── █ █─ █▀▀──── █▀█ █▐█▀ █▀▀ █▀█ █▀▄─ █▀▀ █▐█▀────
+──── ▀ ─▀ ▀▀▀──── ▀▀─ ▀─▀▀ ▀▀▀ ▀─▀ ▀─▀▀ ▀▀▀ ▀─▀▀────".$pth."
+|||=============[".$ijo." author: clarisha".$pth." ]=============|||".$t;
+$msg_ice=$ijo."[•]=[info]".$t.
+"[-] update lagi icebreaker 13 maret tutor facebook ytb clarisha octa".$t;
+
+$stat_ice=true;
+
+$epi=$kn."
+ █▀▀──── █▀▄──── ▀──── ▄▀──── ▄▀▄──── ▄▀▀──── █──
+ █▀▀──── █─█──── █──── █───── █▀█──── ─▀▄──── █▀▄
+ ▀▀▀──── █▀───── ▀──── ─▀──── ▀─▀──── ▀▀───── ▀─▀
+ ".$pth."[ ".$tr."v 1.0".$pth." ]==[ ".$tr."author:".$ijo." clarisha".$pth." ]==[".$tr." ref: ".$ijo."TVFG9W".$pth." ]".$t;
+$msg_epi=$ijo."[•] update skrip https://github.com/clarisha/epicash".$t.$pth."[support:".$tr." khairil".$pth."][Kode: ".$ijo."ZAVGFX]".$t;
+$stat_epi1=true;
+
+$cash= $br."
+ ▄▀ ▄▀▄ ▄▀▀ █────── █───█ ▄▀▄ █── █── █▀▀ ▀█▀
+ █─ █▀█ ─▀▄ █▀▄──── █─█─█ █▀█ █─▄ █─▄ █▀▀ ─█─
+ ─▀ ▀─▀ ▀▀─ ▀─▀──── ─▀─▀─ ▀─▀ ▀▀▀ ▀▀▀ ▀▀▀ ─▀─
+====[".$ijo." version 1.0".$br." ]=[ author:".$ijo." clarisha".$br." ]====".$t;
+$msg_cash=$ijo."[•] enjoy your life";
+$stat_cash=true;
+
+
+$spbu=$pth."
+ ==[ s  p  i  n  n  i  n  g   b  u  c  k  s ]==".$red."
+ _______".$kn."       _____ ".$ijo."      ______ ".$br."      _     _".$red."
+ |______".$kn."      |_____]".$ijo."      |_____]".$br."      |     |".$red."
+ ______|".$kn."      |      ".$ijo."      |_____]".$br."      |_____|".$pth."
+ ======[ ".$tr."v 1.0".$pth." ]====[ ".$tr."author: ".$br."clarisha".$pth." ]======".$t;
+$msg_spbu=$ijo."[•] enjoy your life";
+$stat_spbu=true;
+
+$cbn=$tr."                                                          
+[v 1.0]".$red."_|_|_|".$pth."author".$red."_|_|_|".$ijo." clarisha".$red."_|".$kn." C B N".$red."_|              
+     _|             _|    _|       _|_|    _|              
+     _|             _|_|_|         _|  _|  _|              
+     _|             _|    _|       _|    _|_|              
+     _|_|_|         _|_|_|         _|      _|".$br."           
+_[ T h e ]__".$kn."[ B i t c o i n ]__".$ijo."[ W h e e l ]_".$t;
+$msg_cbn=$ijo."[•] enjoy your life";
+$stat_cbn=true;
+
+$newstom= $br."
+ █▄─█ █▀▀ █───█ ▄▀▀──── ▀█▀ ▄▀▄ █▄─▄█
+ █─▀█ █▀▀ █─█─█ ─▀▄──── ─█─ █─█ █─█─█
+ ▀──▀ ▀▀▀ ─▀─▀─ ▀▀───── ─▀─ ─▀─ ▀───▀".$pth."
+    [v 1.0 ]=[".$tr." author: ".$ijo."clarisha".$pth." ]".$t.$tr;
+$msg_tom=$ijo."[•] enjoy your life";
+$stat_tom=true;
+
+$glu=$ijo."
+____ _    _  _ _ _ _ ____ ____ ___  ____    
+| __ |    |  | | | | |__| |__/ |  \ [__     
+|__] |___ |__| |_|_| |  | |  \ |__/ ___]".$pth."    
+----------------------------------------".$tr."
+[===[ ".$pth."v 1.0".$tr." ]==[".$pth." author:".$ijo." clarisha".$tr." ]===]".$pth."
+----------------------------------------".$t;
+$msg_glu=$ijo."[•] enjoy your life";
+$stat_glu=true;
+
+$yogop= $kuning."
+ ▀▄─▄▀ ▄▀▄ ▄▀▀─ ▄▀▄──── ▐▌─▐▌ ▀ █▀▄ █▀▀ ▄▀▄
+ ──█── █─█ █─▀▌ █─█──── ─▀▄▀─ █ █─█ █▀▀ █─█
+ ──▀── ─▀─ ▀▀▀─ ─▀───── ──▀── ▀ ▀▀─ ▀▀▀ ─▀─".$t.$ijo."[ v ".$v." ] ".$putih."author: ".$ijo."clarisha ".$pth."|| mod: ".$ijo."medusa".$t;
+$msg_yogop=$ijo."[•] info : skrip ini di kelola oleh medusa";
+$stat_yogop=true;
+
+$ecash=$kn."
+──── █▀▀──── ▄▀──── ▄▀▄──── ▄▀▀──── █──────
+──── █▀▀──── █───── █▀█──── ─▀▄──── █▀▄────
+──── ▀▀▀──── ─▀──── ▀─▀──── ▀▀───── ▀─▀────".$pth."
+[ author [".$ijo."clarisha".$pth."]=[ facebook [clarisha octa]".$pth.$t;
+$msg_ecash=$ijo."[•] enjoy your life";
+$stat_ecash=true;
+
+$panda=$ijo."
+──── ▄▀ ▄▀▄ ▄▀▀ █──────".$kn." █▀▄ ▄▀▄ █▄─█ █▀▄ ▄▀▄────".$ijo."
+──── █─ █▀█ ─▀▄ █▀▄────".$kn." █─█ █▀█ █─▀█ █─█ █▀█────".$ijo."
+──── ─▀ ▀─▀ ▀▀─ ▀─▀────".$kn." █▀─ ▀─▀ ▀──▀ ▀▀─ ▀─▀────".$pth."
+=[ author: ".$tr."clarisha".$pth." ]=[ team: ".$br."Black".$red."Coder".$ijo."Crush".$pth."]=".$t;
+$msg_panda="[•]=[ enjoy your life ]";
+$stat_panda=true;
+
+$big=$br."
+──── █▀▄ ▀ ▄▀▀─────".$tr." ▀█▀ ▄▀▄ █─▄▀ █▀▀ █▄─█────".$br."
+──── █▀█ █ █─▀▌────".$tr." ─█─ █─█ █▀▄─ █▀▀ █─▀█────".$br."
+──── ▀▀─ ▀ ▀▀▀─────".$tr." ─▀─ ─▀─ ▀─▀▀ ▀▀▀ ▀──▀────".$pth."
+=| author : ".$ijo."clarisha ".$pth."|==| versi :".$tr." [ 1.0 ]".$pth." |=".$t;
+$msg_big="[•]=[ enjoy your life ]";
+$stat_big=true;
+
+$buzz=$ijo."
+──── █▀▄ █─█ ▀▀▀▀█ ▀▀▀▀█ █▀▄ █▀▀▄ █▀▀ ▄▀▄ █─▄▀────".$kn."
+──── █▀█ █─█ ─▄▀▀─ ─▄▀▀─ █▀█ █▐█▀ █▀▀ █▀█ █▀▄─────".$red."
+──── ▀▀─ ─▀─ ▀▀▀▀▀ ▀▀▀▀▀ ▀▀─ ▀─▀▀ ▀▀▀ ▀─▀ ▀─▀▀────".$pth."
+||| author : ".$ijo."clarisha ".$pth."|||".$br." facebook : ".$tr."clarisha octa ".$pth."|||
+--------------------------------------------------".$t;
+$msg_buzz="[•]=[ enjoy your life ]";
+$stat_buzz=true;
+
+$yodo=$kn."
+──── ▀▄─▄▀──── ▄▀▄──── █▀▄──── ▄▀▄────
+──── ──█────── █─█──── █─█──── █─█────
+──── ──▀────── ─▀───── ▀▀───── ─▀─────".$pth."
+[ author :".$ijo." clarisha".$pth." ]=[ versi :".$ijo." $info ]".$t;
+$msg_yodo=$ijo."[ update skrip v 1.1 add otp all country ]";
+$stat_yodo=true;
+
+$qriket=$ijo."
+_______ ________ ______________ ____________________
+__  __ \___  __ \____  _/___  //_/___  ____/___  __/
+_  / / /__  /_/ / __  /  __  ,<   __  __/   __  /   $kn
+/ /_/ / _  _, _/ __/ /   _  /| |  _  /___   _  /    
+\___\_\ /_/ |_|  /___/   /_/ |_|  /_____/   /_/     $pth
+                                                    
+[ versi:$ijo $vr $pth] [ author :$ijo a d i d o a n k ] ".$t;
+$msg_qr=$ijo."[ enjoy your life ]";
+$stat_qr=true;
+
+?>
