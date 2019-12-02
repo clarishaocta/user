00a2038b7a33e39b4afb58308ce25ddb2811fb62
clarisha octa viany phiranti <57120890+clarishaocta@users.noreply.github.com>
2019-12-02 15:50:26 +0700

    member

---
+++ user.php
@@ -0,0 +1,13 @@
+<?php
+$stat_sseth=true;
+$msgsseth= "[---] enjoy your life";
+$stat_cp = true;
+$msgcp = "[--] enjoy your life";
+$figlet= $ijo."
+───────────────────────────────────────────
+── ▄▀ ▄▀▄ █▀▄ ▀ █▄─█ ▄▀▀ ".$kuning." [`  ||    _    _.       
+── █─ █▀█ █─█ █ █─▀█ █─▀▌".$kuning." | L||| \/(/_|`_\|".$ijo."
+── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t.
+  $putih."creator:".$ijo." clarisha   ".$putih."[--]  ide  :".$ijo." medusa  ".$t.
+  $putih."chanel :".$ijo." clarisha octa ".$putih."[--] chanel:".$ijo." medusa  ".$t.
+  $putih."invite :".$ijo." ipskhdhe2     ".$putih."[--] invite:".$ijo."  v36t2t3   ".$t;
